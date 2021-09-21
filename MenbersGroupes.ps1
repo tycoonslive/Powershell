05e27a6750549800cc6936a0040007fd73a827b0
tycoonslive <piquartlaurent55@gmail.com>
2021-09-21 15:55:06 +0200

    Update MenbersGroupes.ps1

--- MenbersGroupes.ps1
+++ MenbersGroupes.ps1
@@ -1,1 +1,2 @@
-Get-ADGroupMember -Identity "SG_GG_FRA_Commun_2-Deplacements-Chantiers_W" | Select-Object Name | export-csv -path c:\scripts\User_in_ou.csv
+Get-ADGroupMembe -Identity "Groupe Menbers"| Select-Object Name | export-csv -path c:\scripts\User_in_ou.csv
+#Juste change Groupe meber for exoprt user une groupe or secutity groupe wathever#
